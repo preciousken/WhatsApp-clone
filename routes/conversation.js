136a76e4cee2a5c4e2cbd0736fab90bc42c1ce45
preciousken <pkehinde27@gmail.com>
2023-03-28 13:56:33 +0100

    unpin conversation fixed

--- routes/conversation.js
+++ routes/conversation.js
@@ -6,4 +6,7 @@ const { tokenRequired } = require('../middleware/auth')
 // Everything related to pinning a conversation
 conversationRouter.post('/pin',tokenRequired,conversationController.pinChat)
 
+// Everything related to unpinning a conversation
+conversationRouter.post('/unpin',tokenRequired,conversationController.unpinChat)
+
 module.exports = conversationRouter
