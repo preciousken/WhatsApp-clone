b5c3c81a482d83baa0675a35bfeecff0c2101548
preciousken <pkehinde27@gmail.com>
2023-03-30 10:13:37 +0100

    updaetd

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 class Session {
     constructor(){
-        this.chatBrief = document.querySelector('message_body')
+        this.chatBrief = document.querySelector('.message_body')
         // this.log()
         console.log(this.chatBrief);
     }
 
+    // Navigate to chatRoom
+    NavigateToChat(){
+        const home = document.querySelector('.home')
+        home.classList.add('showchatroom')
+        
+    }
 
-    log(){
-        this.chatBrief.addEventListener('click',()=>{
-
-            console.log('clicked');
-        })
-    
+    // Navigate to Message Brief
+    NavigateToMessageBrief(){
+        home.classList.remove('showchatroom')
     }
 
 }
