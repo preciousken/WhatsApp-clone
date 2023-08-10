377484aa0d56c9979020015087a5b69a5f360efc
unknown <pkehinde27@gmail.com>
2023-08-10 18:02:10 +0100

    updated the authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,31 +4,18 @@ const User = require('../model/user');
 
 
 const tokenRequired = async (req,res,next)=>{
-
-
-    // token
     let token = req.headers.whatsapp_token
-
-    // handling error for token
     if(!token){
-        res.status(500).json({
+        return res.status(500).json({
             error : 'TOKEN_ERROR',
             status:false,
             message:`You've got some errors`
         })
-        return;
     }
-
-    // if token exists
-    // verify a token symmetric - synchronous
     try {
         var decoded = jwt.verify(token, process.env.JWT_SECRET);
-        // if token is valid
         if(decoded.data.id){
-            // querying the database for the user
             const user = await User.findById(decoded.data.id);
-
-            // attaching user to the request objec
             req.user = {
                 _id: user._id,
                 email: user.email,
@@ -47,10 +34,7 @@ const tokenRequired = async (req,res,next)=>{
       }
     
 
-    try {
-
-
-        
+    try {  
     } catch (error) {
         res.status(500).json({
             error : 'AUTHENTICATION_ERROR',
