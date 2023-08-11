1000436ef74c69eabf490289a66ac50a3aca4416
unknown <pkehinde27@gmail.com>
2023-08-11 04:12:28 +0100

    made changes to the conversations routes

--- routes/conversation.js
+++ routes/conversation.js
@@ -3,43 +3,36 @@ const conversationRouter = express.Router()
 const conversationController = require('../controller/conversation')
 const { tokenRequired } = require('../middleware/auth')
 
-// Everything related to pinning a conversation
 conversationRouter.post(
 '/pin',
 tokenRequired,
 conversationController.pinChat
 )
 
-// Everything related to unpinning a conversation
 conversationRouter.post(
 '/unpin',
 tokenRequired,
 conversationController.unpinChat
 )
 
-// Everything related to getting list of pinned chats
 conversationRouter.get(
 '/getPinnedChats',
 tokenRequired,
 conversationController.getPinnedChat
 )
 
-// Everything related to achieve chat
 conversationRouter.post(
 '/achieve',
 tokenRequired,
 conversationController.achieveConversation
 )
 
-// Everything related to unachieve chat
 conversationRouter.post(
 '/unachieve',
 tokenRequired,
 conversationController.unAchieveConversation
 )
 
-
-// Everything related to getting list of achieved chats
 conversationRouter.get(
 '/getAchievedChats',
 tokenRequired,
